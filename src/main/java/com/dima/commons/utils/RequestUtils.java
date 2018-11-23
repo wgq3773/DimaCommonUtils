9113f281fab7672749275715a5fb003dccc69e6e
Administrator <Administrator@USER-20171111UK>
2018-11-24 02:45:23 +0800

    DimaCommonUtils

--- src/main/java/com/dima/commons/utils/RequestUtils.java
+++ src/main/java/com/dima/commons/utils/RequestUtils.java
@@ -7,7 +7,13 @@ import javax.servlet.http.HttpServletRequest;
 public class RequestUtils {
 
 	/**
-	 * 从HttpServletRequest中获取请求头信息用于日志打印
+	 * <p>
+	 * Title: getRequestParamts
+	 * </p>
+	 * <p>
+	 * Description: 从HttpServletRequest中获取请求头信息用于日志打印
+	 * </p>
+	 * 
 	 * @param request
 	 * @return
 	 */
@@ -22,5 +28,59 @@ public class RequestUtils {
 			params.append("|");
 		}
 		return params.toString();
+	}
+
+	/**
+	 * <p>
+	 * Title: isFromMobile
+	 * </p>
+	 * <p>
+	 * Description: 根据 Agent判断是否是智能手机
+	 * </p>
+	 * 
+	 * @param request
+	 * @return
+	 */
+	public static Boolean isFromMobile(HttpServletRequest request) {
+		try {
+			Boolean flag = false;
+			String agent = request.getHeader("user-agent");
+			String[] keywords = { "Android", "iPhone", "iPod", "iPad", "Windows Phone", "MQQBrowser" };
+			// 排除 Windows 桌面系统
+			if (!agent.contains("Windows NT")
+					|| (agent.contains("Windows NT") && agent.contains("compatible; MSIE 9.0;"))) {
+				// 排除 苹果桌面系统
+				if (!agent.contains("Windows NT") && !agent.contains("Macintosh")) {
+					for (String item : keywords) {
+						if (agent.contains(item)) {
+							flag = true;
+							break;
+						}
+					}
+				}
+			}
+			return flag;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	/**
+	 * <p>
+	 * Title: isWechat
+	 * </p>
+	 * <p>
+	 * Description: 判断是否微信浏览器
+	 * </p>
+	 * 
+	 * @param request
+	 * @return
+	 */
+	public static boolean isWechat(HttpServletRequest request) {
+		String ua = request.getHeader("User-Agent").toLowerCase();
+		if (ua.indexOf("micromessenger") > -1) {
+			return true;// 微信
+		}
+		return false;// 非微信手机浏览器
 	}
 }
